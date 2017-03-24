f1416bd0c10737038d0541e30989b7a8aa3978ad
Vincent Rossignol <rossignol.vince@gmail.com>
2017-03-24 15:06:46 +0100

    Refactoring

--- public/App.jsx
+++ public/App.jsx
@@ -6,10 +6,6 @@ class App extends React.Component {
 
 	constructor(props) {
 		super(props);
-
-		this.state = {
-			navbarHeight: 100
-		};
 	}
 
 	render() {
@@ -18,23 +14,8 @@ class App extends React.Component {
 				<div id="left-column" className="column medium-2">
 					
 				</div>
-				<div id="main-content" className="column medium-10">
-					<div className="expanded row" style={{height: this.state.navbarHeight + "px"}}>
-						<div className="column medium-3" style={{height: "100%", border: "1px solid yellow"}}>
-							
-						</div>
-						<div className="column medium-9" style={{height: "100%", border: "1px solid violet"}}>
-
-						</div>
-					</div>
-
-					<div className="expanded row" style={{height: "calc(100% - " + this.state.navbarHeight + "px)"}}>
-						<Circle />
-						<div className="column medium-3" style={{height: "100%", border: "1px solid orange"}}>
-
-						</div>
-					</div>
-				</div>
+                
+                <Circle />
 			</div>
 		);
 	}
--- public/Circle.jsx
+++ public/Circle.jsx
@@ -7,13 +7,29 @@ class Circle extends React.Component {
 		super(props);
         
         this.state = {
-            group: "Groupe 1"
+            group: "Groupe 1",
+            navbarHeight: 100
         };
     }
     
     render() {
         return (
-            <Line />
+            <div className="column medium-10" style={{height: "100%"}}>
+                <div className="expanded row" style={{height: this.state.navbarHeight + "px"}}>
+                    <div className="column medium-3" style={{height: "100%", border: "1px solid yellow"}}>
+
+                    </div>
+                    <div id="banner" className="column medium-9" style={{height: "100%", border: "1px solid violet", }}>
+                    </div>
+                </div>
+
+                <div className="row" style={{height: "calc(100% - " + this.state.navbarHeight + "px"}}>
+                    <Line />
+
+                    <div className="column medium-3" style={{height: "100%", border: "1px solid orange"}}>
+                    </div>
+                </div>
+            </div>
         );
     }
 }

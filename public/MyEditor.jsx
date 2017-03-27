a0696f5f0e0e581f3c4c71feed32e14c01ac6e16
Vincent Rossignol <rossignol.vince@gmail.com>
2017-03-27 07:06:58 +0200

    MyEditor.jsx added

---
+++ public/MyEditor.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import {Editor, EditorState, RichUtils} from 'draft-js';
+
+export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {editorState: EditorState.createEmpty()};
+        this.onChange = (editorState) => this.setState({editorState});
+    }
+    
+    _onBoldClick() {
+        this.onChange(RichUtils.toggleInlineStyle(
+        this.state.editorState,
+        'BOLD'
+        ));
+    }
+  
+    render() {
+        return (
+        <div id="content">
+            <button onClick={this._onBoldClick.bind(this)}>Bold</button>
+            <div className="editor">
+                <Editor id="editor" editorState={this.state.editorState} onChange={this.onChange}/>
+            </div>
+        </div>
+        );
+    }
+}

00cbb393dc7ecfc5da9b50c3d86bbb3808249b56
Vincent Rossignol <rossignol.vince@gmail.com>
2017-04-07 13:04:36 +0200

    Styling chat & added loading mechanism for Circle

---
+++ public/Navigator.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+class Navigator extends React.Component {
+    
+    constructor(props) {
+		super(props);
+        this.state = {
+            circles: props.circles,
+            users: props.users
+        }
+    }
+    
+    render() {
+        return (<div id="left-column" className="column medium-2">
+                    <div id="search" className="row">
+                        <input type="text" id="search-text" className="column medium-10" />
+                        <button type="button" id="search-button" className="column medium-2 button">Go!</button>
+                    </div>
+                    <ul className="navigationList" style={{height: "40%"}}> 
+                        {
+                            this.state.circles.map(function(circle){
+                                return <div key={circle.id} className="row circleListItem">{circle.name}</div>
+                            })
+                        }
+                    </ul>
+                    <hr></hr>
+                    <ul className="navigationList" style={{height: "40%"}}> 
+                        {
+                            this.state.users.map(function(user){
+                                return <div key={user.id} className="row circleListItem">{user.name}</div>
+                            })
+                        }
+                    </ul>
+					
+				</div>)
+    }
+}
+
+export default Navigator;

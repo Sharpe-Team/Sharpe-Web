6fd0a33b1d250de27d75ee6c73d6b1b0914650b3
Vincent Rossignol <rossignol.vince@gmail.com>
2017-05-15 16:22:07 +0200

    styling fix

--- public/components/app/chat/NavigatorMenu.jsx
+++ public/components/app/chat/NavigatorMenu.jsx
@@ -5,9 +5,10 @@ class NavigatorMenu extends React.Component {
     render(){
         return (
             <div id="search" className="row">
-                <Link  to="/userform"><img className="user-form-button" src="/resource/user.png"></img></Link>
-                <Link  to="/circleform"><img className="circle-form-button" src="/resource/circle.png"></img></Link>
-                <Link  to="/logout"><img className="logout-form-button" src="/resource/logout.png"></img></Link>
+                <Link  to="/"><img className="nav-button" src="/resource/circlehome.png"/></Link>
+                <Link  to="/userform"><img className="nav-button" src="/resource/user.png"></img></Link>
+                <Link  to="/circleform"><img className="nav-button" src="/resource/circle.png"></img></Link>
+                <Link  to="/logout"><img className="nav-button" src="/resource/logout.png"></img></Link>
             </div>
         );
     }

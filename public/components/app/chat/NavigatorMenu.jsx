0d8b1c0751a0e33043980b608320338add7b337f
Vincent Rossignol <rossignol.vince@gmail.com>
2017-05-15 17:34:02 +0200

    removed circle page menu

--- public/components/app/chat/NavigatorMenu.jsx
+++ public/components/app/chat/NavigatorMenu.jsx
@@ -5,7 +5,6 @@ class NavigatorMenu extends React.Component {
     render(){
         return (
             <div id="search" className="row">
-                <Link  to="/"><img className="nav-button" src="/resource/circlehome.png"/></Link>
                 <Link  to="/userform"><img className="nav-button" src="/resource/user.png"></img></Link>
                 <Link  to="/circleform"><img className="nav-button" src="/resource/circle.png"></img></Link>
                 <Link  to="/logout"><img className="nav-button" src="/resource/logout.png"></img></Link>
